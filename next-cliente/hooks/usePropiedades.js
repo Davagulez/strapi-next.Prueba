bf8b8fa969d63585c2aae1e59c0964df7d3757d5
David Lezcano <lezcadav@gmail.com>
2023-07-17 16:51:54 -0300

    feat: add nuevo usuario para manejar el CRUD

---
+++ next-cliente/hooks/usePropiedades.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { styled } from "@emotion/styled";
+
+const usePropiedades = () => {
+    const Propiedades = () => {
+            <h1>Desde componente Propiedades</h1>
+    }
+    return {
+        Propiedades
+    }
+}
+
+export default usePropiedades;
